1d7ec0b7c6ad88c6f0b2eeae7fb1a3a055547e27
Matthias Quade <mquade@technologiewerk-qua.de>
2023-11-10 20:26:14 +0100

    Update serverInstall.sh

--- serverInstall.sh
+++ serverInstall.sh
@@ -50,13 +50,13 @@ EOT
 
 apt update
 apt dist-upgrade -y
-apt install -y man apt-transport-https lsb-release ca-certificates curl wget dirmngr htop screen unzip nano vim-nox mc git multitail dos2unix python3-pip dnsutils whois lvm2 ufw rsync gnupg pwgen
+apt install -y man apt-transport-https lsb-release ca-certificates curl wget dirmngr htop screen unzip nano vim-nox mc git multitail dos2unix python3-pip dnsutils whois lvm2 ufw rsync gnupg pwgen sudo
 
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 # apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 apt update
